6d7a6ad5176198b1721ec6d99016eef6fb875396
Rob McQueen <rob@segment.com>
2019-09-11 17:34:44 -0700

    ive never had to work so hard to add a favico

---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+        <link rel="shortcut icon" type="image/x-icon" href="/static/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
