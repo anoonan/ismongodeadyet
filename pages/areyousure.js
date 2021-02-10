9fec56a207ea2adcdd4bae573974cf6f6bc32a4f
Scott Hardy <scott.the.hardy@gmail.com>
2021-02-09 21:21:08 -0800

    Add confetti to /areyousure (#10)

--- pages/areyousure.js
+++ pages/areyousure.js
@@ -1,6 +1,7 @@
 export default function Areyousure() {
   return (
     <div>
+      <script src="/static/confetti.js"></script>
       <p><font size="+10"><center>Yes, I'm sure</center></font></p>
     </div>
   );
